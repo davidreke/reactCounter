441e777842568b5a46b481a6ed07edc43fa2f6bf
David Reke <david.e.reke@gmail.com>
2020-08-04 17:20:38 -0400

    Updates

---
+++ precache-manifest.24d467dcebda626cab4d42dce8cd350b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "813e6823eab36d0a5a3a3ffdcc302b72",
+    "url": "/reactCounter/index.html"
+  },
+  {
+    "revision": "227a762479c7b63e36b7",
+    "url": "/reactCounter/static/css/main.38491124.chunk.css"
+  },
+  {
+    "revision": "9f0cf07da277f0734a2a",
+    "url": "/reactCounter/static/js/2.9330d977.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactCounter/static/js/2.9330d977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "227a762479c7b63e36b7",
+    "url": "/reactCounter/static/js/main.6ce373a7.chunk.js"
+  },
+  {
+    "revision": "6b2d9594fdd87ca1295c",
+    "url": "/reactCounter/static/js/runtime-main.a75fe4e8.js"
+  }
+]);
